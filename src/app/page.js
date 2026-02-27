bfe10eefdd1ae67300658be1c2c38772f9bfc756
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-02-27 13:47:46 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>Aula de introdução</h2>
+      <h3>Leiaute e Estilização</h3>
+      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
     </div>
   );
 }
