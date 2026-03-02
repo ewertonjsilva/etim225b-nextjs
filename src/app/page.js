ef53fc8826c1142f08dcbab17cad5c69264f6aee
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-02 15:25:14 -0300

    aula posicionamento site projeto css

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,17 @@ import styles from "./page.module.css";
 
 function Home() {
   return (
-    <div>
+    <div style={{border: '2px solid #ff0', display: 'flex', flexDirection: 'column'}}>
       <h1>React com Next JS</h1>
       <h2>Aula de introdução</h2>
       <h3>Leiaute e Estilização</h3>
       <p>Nesta aula iremos entender o uso de CSS global e local.</p>
+      <label>Exemplo Label</label>
+      <small>Exemplo Small</small>
+      <div style={{border: '2px solid #00f', display: 'flex', flexDirection: 'column'}}>
+        <div style={{ height: '30px', width: '30px', backgroundColor: '#f00' }}></div>
+        <div style={{ height: '30px', width: '30px', backgroundColor: '#0f0' }}></div>
+      </div>
     </div>
   );
 }
