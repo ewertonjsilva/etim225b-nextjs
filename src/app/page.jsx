7322cfc44dfa5219c798246d81628e6bc2a73e83
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-06 11:45:25 -0300

    aula organização projeto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,16 @@ import styles from "./page.module.css";
 
 function Home() {
   return (
-    <div style={{border: '2px solid #ff0', display: 'flex', flexDirection: 'column'}}>
+    <div className={styles.containerExemplo}>
       <h1>React com Next JS</h1>
       <h2>Aula de introdução</h2>
       <h3>Leiaute e Estilização</h3>
       <p>Nesta aula iremos entender o uso de CSS global e local.</p>
       <label>Exemplo Label</label>
       <small>Exemplo Small</small>
-      <div style={{border: '2px solid #00f', display: 'flex', flexDirection: 'column'}}>
-        <div style={{ height: '30px', width: '30px', backgroundColor: '#f00' }}></div>
-        <div style={{ height: '30px', width: '30px', backgroundColor: '#0f0' }}></div>
+      <div className={styles.conteinerExPosicao}>
+        <div className={styles.divVermelha}></div>
+        <div className={styles.divVerde}></div>
       </div>
     </div>
   );
