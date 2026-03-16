ccd7afbd7f1b66c2a28be6a09d83f8bb95347e20
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-16 15:18:26 -0300

    aula 16-03

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,7 +1,9 @@
+import styles from './page.module.css';
+
 function Atividade01 () {
     return(
         <div>
-            <h1>Atividade 1</h1>
+            <h1 className={styles.tituloAtv}>Atividade 1</h1>
             <p>Componente básico para renderizar tela com React</p>
         </div>
     )
