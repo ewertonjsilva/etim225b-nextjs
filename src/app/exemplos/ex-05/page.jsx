871c5f16ff6e2b50c727a4022a0909ad4480558e
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-04-13 15:22:32 -0300

    aula 13-04

--- src/app/exemplos/ex-05/page.jsx
+++ src/app/exemplos/ex-05/page.jsx
@@ -1,29 +1,49 @@
 'use client'
 
-import { useState } from 'react';
+import { useState } from 'react'; 
+
+import styles from './page.module.css';
 
 export default function FormComponent() {
 
     // const [formData, setFormData] = useState({ nome: '', email: '' });
+    const [login, setLogin] = useState('');
+    const [senha, setSenha] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Dados do formulário enviados!');
-        
+        console.log(`Usuário: ${login} \nSenha: ${senha}`);
         // console.log("Enviando para API:", formData);
         // Aqui você faria um POST para sua API
     };
+    
 
     return (
-        <form onSubmit={handleSubmit} style={{ maxWidth: '400px' }}>
-            <label htmlFor="user">Nome do Usuário:</label>
-            <input
-                id="user"
-                type="text"
+        <div className={styles.container}>
+
+            <h1>Exemplo 5 - Formulário</h1>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit} className={styles.formulario}>                
+                <input
+                    name="login"
+                    type="text" 
+                    placeholder='usuário'
+                    onChange={ e => setLogin(e.target.value)}
                 // value={formData.nome}
                 // onChange={(e) => setFormData({ ...formData, nome: e.target.value })}
-            />
-            <button type="submit">Enviar Cadastro</button>
-        </form>
+                />
+                <input
+                    name="senha"
+                    type="password" 
+                    placeholder='senha' 
+                    onChange={ e => setSenha(e.target.value)}
+                // value={formData.nome}
+                // onChange={(e) => setFormData({ ...formData, nome: e.target.value })}
+                />
+                <button type="submit">Acessar sistema</button>
+            </form>
+        </div>
     );
 }
