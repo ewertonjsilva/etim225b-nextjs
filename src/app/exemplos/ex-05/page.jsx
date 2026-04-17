306898cc643347a5abebdc302f6504e5b15a3666
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-04-17 11:40:42 -0300

    aula 17-04 -- manha

--- src/app/exemplos/ex-05/page.jsx
+++ src/app/exemplos/ex-05/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'; 
+import { use, useState } from 'react';
 
 import styles from './page.module.css';
 
@@ -10,14 +10,42 @@ export default function FormComponent() {
     const [login, setLogin] = useState('');
     const [senha, setSenha] = useState('');
 
+    // Array histórico de logins
+    const [historico, setHistorico] = useState([]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Dados do formulário enviados!');
-        console.log(`Usuário: ${login} \nSenha: ${senha}`);
+
         // console.log("Enviando para API:", formData);
         // Aqui você faria um POST para sua API
+
+        // if (login && senha) {
+        //     console.log('Dados do formulário enviados!');
+        //     console.log(`Usuário: ${login} \nSenha: ${senha}`);
+        
+        //     let historicoTemp = historico;
+
+        //     // Exemplo horário
+        //     const horario = new Date();
+        //     historicoTemp.push(`Usuário: ${login} - Horário ${horario.toLocaleString()}`);
+
+        //     setHistorico(historicoTemp);
+        //     // console.log(historico);
+
+        // }
+
+        if (login && senha) {
+        const horario = new Date();
+        const novaEntrada = `Usuário: ${login} - Horário ${horario.toLocaleString()}`;
+
+        // Cria um NOVO array com os elementos antigos + o novo
+        // setHistorico([novaEntrada]);
+        setHistorico([...historico, novaEntrada]);
+    }
     };
-    
+
+
+
 
     return (
         <div className={styles.container}>
@@ -25,25 +53,36 @@ export default function FormComponent() {
             <h1>Exemplo 5 - Formulário</h1>
             <h2>Login</h2>
 
-            <form onSubmit={handleSubmit} className={styles.formulario}>                
+            <form onSubmit={handleSubmit} className={styles.formulario}>
                 <input
                     name="login"
-                    type="text" 
+                    type="text"
                     placeholder='usuário'
-                    onChange={ e => setLogin(e.target.value)}
+                    onChange={e => setLogin(e.target.value)}
                 // value={formData.nome}
                 // onChange={(e) => setFormData({ ...formData, nome: e.target.value })}
                 />
                 <input
                     name="senha"
-                    type="password" 
-                    placeholder='senha' 
-                    onChange={ e => setSenha(e.target.value)}
+                    type="password"
+                    placeholder='senha'
+                    onChange={e => setSenha(e.target.value)}
                 // value={formData.nome}
                 // onChange={(e) => setFormData({ ...formData, nome: e.target.value })}
                 />
                 <button type="submit">Acessar sistema</button>
             </form>
+
+            <div className={styles.historico}>
+                <h3>Histórico de Logins</h3>
+                
+                {
+                    historico.map( item => 
+                        <p key={item}>{item}</p>
+                    )
+                }
+
+            </div>
         </div>
     );
 }
